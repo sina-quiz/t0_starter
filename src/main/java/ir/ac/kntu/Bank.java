b85be8888c8f42f40289f177e6a4a72e7495a6a2
Sina Marefat <Sinamatefat@gmail.com>
2021-08-29 21:40:42 +0430

    Quiz-0

--- src/main/java/ir/ac/kntu/Bank.java
+++ src/main/java/ir/ac/kntu/Bank.java
@@ -5,25 +5,5 @@ public class Bank {
     public Bank() {
     }
 
-    public Boolean payWithCash(Costumer costumer,int cost){
-        if(cost>costumer.getCash()){
-            return false;
-        } else{
-            costumer.setCash(costumer.getCash()-cost);
-            costumer.totalCalculator();
-            return true;
-        }
-    }
-
-    public Boolean payWithCredit(Costumer costumer,int cost){
-        if((cost/2)>costumer.getCreditCard()){
-            return false;
-        } else{
-            costumer.setCreditCard(costumer.getCreditCard()-(cost/2));
-            costumer.totalCalculator();
-            return true;
-        }
-
-    }
 }
 
